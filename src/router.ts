1b7a378eba5ab874adf22628d7ce272db916441f
Halil Uzer <haliluzer@outlook.com>
2023-05-12 23:19:59 +0300

    sign in update

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,13 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from './views/HomeView.vue'
 import SignInView from './views/SignInView.vue'
 import SignUpView from './views/SignUpView.vue'
+import ProfileView from '@/views/'
 
 const routes = [
   { path: '/sign-in', name: 'sign-in', component: SignInView },
   {path: '/', name: 'home', component: HomeView},
-  {path: '/sign-up', name: 'sign-up', component: SignUpView}
+  {path: '/sign-up', name: 'sign-up', component: SignUpView},
+  {path: '/profile', name:'profile', component: ProfileView}
 ]
 
  const router = createRouter({
