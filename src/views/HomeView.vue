45b8795e5f8900b8f3922f95246b886a17f9e276
Halil Uzer <haliluzer@outlook.com>
2023-05-13 23:57:03 +0300

    profile update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import useTokenStore from '@/stores/tokenStore';
 import Navbar from './../components/Navbar.vue'
-import useUsernameStore from '@/stores/usernameStore';
-
-const tokenStorage = useTokenStore()
-const usernameStorage = useUsernameStore()
 </script>
 
 <style></style>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,19 +1,37 @@
 <script setup lang="ts">
 import useUsernameStore from '@/stores/usernameStore';
 import useEmailStore from '@/stores/emailStore';
+import Navbar from '@/components/Navbar.vue';
 
 const emailStore = useEmailStore();
 const usernameStore = useUsernameStore();
 </script>
 
 <template>
-    <div class="row">
-        <div class="col-lg-4">
-            <div class="card mb-4">
-                <div class="card-body text-center">
-                    <h5 class="my-3">{{ usernameStore.getUsername }}</h5>
+    <Navbar />
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 mt-5 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <h5 class="my-3">{{ usernameStore.getUsername.value }}</h5>
+                        <h5 class="my-3">{{ emailStore.getEmail.value }}</h5>
+                    </div>
                 </div>
+                
             </div>
+            <div class="row justify-content-center">
+                    <div class="col-auto">
+                        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+                            <label class="btn btn-outline-primary" for="btnradio1">Wallet</label>
+                            <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                            <label class="btn btn-outline-primary" for="btnradio2">Owned NFTs</label>
+                            <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                            <label class="btn btn-outline-primary" for="btnradio3">NFTs on Market</label>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
 </template>
