149c9167673a69911ce3d163ecfedd56b24f79fa
Halil Uzer <haliluzer@outlook.com>
2023-05-16 22:23:15 +0300

    profile page update

--- src/components/WalletCard.vue
+++ src/components/WalletCard.vue
@@ -6,7 +6,7 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-sm-3">
-                            <p class="mb-0">Full Name</p>
+                            <p class="mb-0">Wallet Address</p>
                         </div>
                         <div class="col-sm-9">
                             <p class="text-muted mb-0">Johnatan Smith</p>
@@ -15,42 +15,16 @@
                     <hr>
                     <div class="row">
                         <div class="col-sm-3">
-                            <p class="mb-0">Email</p>
+                            <p class="mb-0">Balance</p>
                         </div>
                         <div class="col-sm-9">
                             <p class="text-muted mb-0">example@example.com</p>
                         </div>
                     </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-sm-3">
-                            <p class="mb-0">Phone</p>
-                        </div>
-                        <div class="col-sm-9">
-                            <p class="text-muted mb-0">(097) 234-5678</p>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-sm-3">
-                            <p class="mb-0">Mobile</p>
-                        </div>
-                        <div class="col-sm-9">
-                            <p class="text-muted mb-0">(098) 765-4321</p>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-sm-3">
-                            <p class="mb-0">Address</p>
-                        </div>
-                        <div class="col-sm-9">
-                            <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
-                        </div>
-                    </div>
-                </div>
+
             </div>
         </div>
+    </div>
     </div>
 </template>
 
