1d36d54f8c0a9e5b162c71491ac4a56552a5ad1e
Halil Uzer <haliluzer@outlook.com>
2023-06-14 20:08:18 +0300

    nft view update

--- src/views/NftView.vue
+++ src/views/NftView.vue
@@ -7,6 +7,7 @@ import type { AxiosResponse } from 'axios';
 import useUsernameStore from '@/stores/usernameStore';
 import useTokenStore from '@/stores/tokenStore';
 import useUserIdStore from '@/stores/useridStore';
+import router from '@/router';
 
 const usernameStore = useUsernameStore()
 const tokenStore = useTokenStore();
@@ -17,21 +18,17 @@ let nftImgSrc = reactive({ value: '' });
 let nftPrice = reactive({ value: 0 });
 let nftOwnerUsername = reactive({ value: '' });
 let onSaleMessage = reactive({ value: 'Not On Sale', color: 'red' })
-let isOnSale = reactive({ value: false, message: 'Put On Sale' });
-
+let isOnSale = reactive({ value: false });
+let isMinted = reactive({ value: false });
 let displayOwnerOptions = reactive({ value: false });
 let newNftPrice = reactive({ value: 0 });
 const route = useRoute();
 
-async function putOnSale() {
+async function setPrice() {
     try {
-        let response = await axios.put(`http://127.0.0.1:4000/nfts/${route.params.nftId}/put-on-sale/${newNftPrice}`);
+        let response = await axios.put(`http://127.0.0.1:4000/nfts/${route.params.nftId}/set-price/${newNftPrice.value}`);
         if (response.status === 200) {
-            nftPrice.value = response.data['price'];
-            onSaleMessage.value = 'On Sale';
-            onSaleMessage.color = '#00e600';
-            isOnSale.message = 'Set Price'
-            isOnSale.value = true;
+            nftPrice.value = response.data['price'] 
         }
     }
     catch (err) {
@@ -39,10 +36,28 @@ async function putOnSale() {
     }
 }
 
+
+async function putOnSale() {
+    try {
+        let response = await axios.put(`http://127.0.0.1:4000/nfts/${route.params.nftId}/put-on-sale`);
+        if (response.status === 200) {
+            onSaleMessage.value = 'On Sale';
+            onSaleMessage.color = '#00e600';
+            isOnSale.value = true;
+        }
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
 async function buy() {
     console.log(userIdStore.getUserId)
     try {
-        let response = await axios.post(`http://127.0.0.1:4000/nfts/buy/${route.params.nftId}`, {buyerId: userIdStore.getUserId}, { headers: { Authorization: `Bearer ${tokenStore.getToken}` } });
+        let response = await axios.post(`http://127.0.0.1:4000/nfts/buy/${route.params.nftId}`, { buyerId: userIdStore.getUserId }, { headers: { Authorization: `Bearer ${tokenStore.getToken}` } });
+        if(response.status === 200){
+            router.go(0)
+        }
     }
     catch (err) {
         console.log(err);
@@ -58,7 +73,9 @@ onMounted(async () => {
         nftName.value = response.data['nftName'];
         nftImgSrc.value = response.data['nftUrl'];
         nftOwnerUsername.value = response.data['nftOwner'];
-        isOnSale.value = response.data['nftIsOnSale'] as boolean;
+        isMinted.value = response.data['isMinted'];
+        isOnSale.value = response.data['isNftOnSale'] as boolean;
+        
         if (nftOwnerUsername.value === usernameStore.getUsername.value) {
             displayOwnerOptions.value = true;
         }
@@ -69,13 +86,13 @@ onMounted(async () => {
         if (isOnSale.value === true) {
             onSaleMessage.value = 'On Sale';
             onSaleMessage.color = '#00e600';
-            isOnSale.message = 'Set Price'
         }
         else {
             onSaleMessage.value = 'Not On Sale';
             onSaleMessage.color = 'red';
-            isOnSale.message = 'Put On Sale';
         }
+
+
     }
     catch (err) {
         console.log(err);
@@ -92,14 +109,17 @@ onMounted(async () => {
             <h2>{{ nftName.value }}</h2>
             <h4>Owned by {{ nftOwnerUsername.value }}</h4>
             <h5>{{ nftPrice.value }} ETH</h5>
+            <h5 v-if="isMinted.value">Minted</h5>
             <h6 :color="onSaleMessage.color" v-if="displayOwnerOptions.value">{{ onSaleMessage.value }}</h6>
             <button type="button" class="btn btn-primary mb-3"
                 v-if="tokenStore.isAuthenticated && !displayOwnerOptions.value" @click="buy">Buy</button>
             <button class="btn btn-primary mb-3" href="#" role="button" v-if="displayOwnerOptions.value"
-                @click="putOnSale">{{ isOnSale.message }}</button>
+                @click="setPrice">Set Price</button>
             <div class="form-floating mb-3" v-if="displayOwnerOptions.value">
-                <input type="number" class="form-control" id="floatingInput" placeholder="0" v-model="newNftPrice">
+                <input type="number" class="form-control mb-3" id="floatingInput" placeholder="0" v-model="newNftPrice.value">
                 <label for="floatingInput">Price</label>
+                <button class="btn btn-primary mt-3" href="#" role="button" v-if="displayOwnerOptions.value"
+                    @click="putOnSale">Put On Sale</button>
             </div>
         </div>
     </div>
